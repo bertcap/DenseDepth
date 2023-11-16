18c301fbcf8fbff6b03e5126c6d77c6c70b07cd1
Bert Cappelle <bert.cappelle@ugent.be>
2023-11-16 11:42:54 +0100

    changed test file to gray colors

--- test.py
+++ test.py
@@ -2,6 +2,10 @@ import os
 import glob
 import argparse
 import matplotlib
+import sys
+import numpy as np
+import cv2
+from PIL import Image
 
 # Keras / TensorFlow
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '5'
@@ -11,11 +15,9 @@ from tensorflow.keras.layers import Layer, InputSpec
 from utils import predict, load_images, display_images
 from matplotlib import pyplot as plt
 
+model_file = 'nyu.h5'
 # Argument Parser
-parser = argparse.ArgumentParser(description='High Quality Monocular Depth Estimation via Transfer Learning')
-parser.add_argument('--model', default='nyu.h5', type=str, help='Trained Keras model file.')
-parser.add_argument('--input', default='examples/*.png', type=str, help='Input filename or folder.')
-args = parser.parse_args()
+image_path = sys.argv[1]
 
 # Custom object needed for inference and training
 custom_objects = {'BilinearUpSampling2D': BilinearUpSampling2D, 'depth_loss_function': None}
@@ -23,23 +25,42 @@ custom_objects = {'BilinearUpSampling2D': BilinearUpSampling2D, 'depth_loss_func
 print('Loading model...')
 
 # Load model into GPU / CPU
-model = load_model(args.model, custom_objects=custom_objects, compile=False)
+model = load_model(model_file, custom_objects=custom_objects, compile=False)
 
-print('\nModel loaded ({0}).'.format(args.model))
+print('\nModel loaded ({0}).'.format(model_file))
 
 # Input images
-inputs = load_images( glob.glob(args.input) )
-print('\nLoaded ({0}) images of size {1}.'.format(inputs.shape[0], inputs.shape[1:]))
+image = cv2.imread(image_path)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+orig_width = image.shape[1]
+orig_height = image.shape[0]
+resized_image = cv2.resize(image, dsize=(640, 480), interpolation=cv2.INTER_AREA)
+x = [np.clip(np.asarray(resized_image, dtype=float) / 255, 0, 1)]
+input = np.stack(x, axis=0)
+print('\nLoaded image of size {0}.'.format(input.shape[0:]))
 
 # Compute results
-outputs = predict(model, inputs)
+output = predict(model, input)
+output_resized = cv2.resize(output[0], (orig_width, orig_height), interpolation=cv2.INTER_CUBIC)
+output_rescaled = cv2.normalize(output_resized, None, 0, 255, cv2.NORM_MINMAX)
+new_pathname = image_path.replace('.', '_depthmap.')
+cv2.imwrite(new_pathname, output_rescaled) 
 
-#matplotlib problem on ubuntu terminal fix
-#matplotlib.use('TkAgg')   
 
-# Display results
-viz = display_images(outputs.copy(), inputs.copy())
-plt.figure(figsize=(10,5))
-plt.imshow(viz)
-plt.savefig('test.png')
+fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+
+# Display first image on the first axis
+axs[0].imshow(image)
+axs[0].set_title('Original image')
+
+# Display the second image (array) on the second axis
+axs[1].imshow(output_rescaled, cmap='gray')  # Use cmap='gray' for grayscale images
+axs[1].set_title('Depth Map')
+
+# Hide axis ticks and labels
+for ax in axs:
+    ax.axis('off')
+
+# Show the images side by side
+plt.tight_layout()
 plt.show()
