2bd62e799bb46b1ff35054f3b84f72f783a8b170
Bert Cappelle <bert.cappelle@ugent.be>
2023-11-27 10:08:26 +0100

    Inverted depth map colors

--- test.py
+++ test.py
@@ -1,51 +1,54 @@
 import os
-import glob
-import argparse
-import matplotlib
 import sys
 import numpy as np
 import cv2
-from PIL import Image
+import time
 
-# Keras / TensorFlow
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '5'
 from keras.models import load_model
 from layers import BilinearUpSampling2D
-from tensorflow.keras.layers import Layer, InputSpec
-from utils import predict, load_images, display_images
+from utils import predict
 from matplotlib import pyplot as plt
 
-model_file = 'nyu.h5'
+# Set TensorFlow logging level
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '5'
+
 # Argument Parser
 image_path = sys.argv[1]
+model_file = 'nyu.h5'
 
 # Custom object needed for inference and training
 custom_objects = {'BilinearUpSampling2D': BilinearUpSampling2D, 'depth_loss_function': None}
 
 print('Loading model...')
+start_time_model = time.time()
 
 # Load model into GPU / CPU
 model = load_model(model_file, custom_objects=custom_objects, compile=False)
 
-print('\nModel loaded ({0}).'.format(model_file))
+end_time_model = time.time()
+print(f'\nModel loaded ({model_file}). Time taken: {end_time_model - start_time_model:.4f} seconds.')
 
 # Input images
 image = cv2.imread(image_path)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-orig_width = image.shape[1]
 orig_height = image.shape[0]
+orig_width = image.shape[1]
 resized_image = cv2.resize(image, dsize=(640, 480), interpolation=cv2.INTER_AREA)
-x = [np.clip(np.asarray(resized_image, dtype=float) / 255, 0, 1)]
-input = np.stack(x, axis=0)
-print('\nLoaded image of size {0}.'.format(input.shape[0:]))
+input = np.clip(np.asarray(resized_image, dtype=float) / 255, 0, 1)
+#print('\nLoaded image of size {0}.'.format(input.shape[0:]))
+
+start_time_prediction = time.time()
 
 # Compute results
 output = predict(model, input)
+
+end_time_prediction = time.time()
+print(f'Prediction completed. Time taken: {end_time_prediction - start_time_prediction:.4f} seconds.')
+
 output_resized = cv2.resize(output[0], (orig_width, orig_height), interpolation=cv2.INTER_CUBIC)
 output_rescaled = cv2.normalize(output_resized, None, 0, 255, cv2.NORM_MINMAX)
 new_pathname = image_path.replace('.', '_depthmap.')
-cv2.imwrite(new_pathname, output_rescaled) 
-
+cv2.imwrite(new_pathname, output_rescaled)
 
 fig, axs = plt.subplots(1, 2, figsize=(10, 5))
 
--- utils.py
+++ utils.py
@@ -4,14 +4,14 @@ from PIL import Image
 def DepthNorm(x, maxDepth):
     return maxDepth / x
 
-def predict(model, images, minDepth=10, maxDepth=1000, batch_size=2):
+def predict(model, images, minDepth=10, maxDepth=1000, batch_size=5):
     # Support multiple RGBs, one RGB image, even grayscale 
     if len(images.shape) < 3: images = np.stack((images,images,images), axis=2)
     if len(images.shape) < 4: images = images.reshape((1, images.shape[0], images.shape[1], images.shape[2]))
     # Compute predictions
     predictions = model.predict(images, batch_size=batch_size)
     # Put in expected range
-    return np.clip(DepthNorm(predictions, maxDepth=maxDepth), minDepth, maxDepth) / maxDepth
+    return predictions
 
 def scale_up(scale, images):
     from skimage.transform import resize
