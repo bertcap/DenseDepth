03f1a818685717149fbf6b140dfd3452139c6c66
Bert Cappelle <bert.cappelle@ugent.be>
2023-12-10 11:08:52 +0100

    Added test images

--- resize.py
+++ resize.py
@@ -1,4 +1,4 @@
 import cv2
-image = cv2.imread("examples/bert.jpg")
-resized_image = cv2.resize(image, dsize=(1000, 691), interpolation=cv2.INTER_AREA)
-cv2.imwrite("examples/bert.jpg", resized_image)
+image = cv2.imread("examples/test_image_3.jpg")
+resized_image = cv2.resize(image, dsize=(504, 378), interpolation=cv2.INTER_AREA)
+cv2.imwrite("examples/test_image_3.jpg", resized_image)
